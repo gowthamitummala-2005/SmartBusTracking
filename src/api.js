3edd41348e5d37f3145020599f2a9d7377138b73
gowthamitummala-2005 <23eg110c61@anurag.edu.in>
2026-04-04 19:07:16 +0530

    message

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smartbus-backend-url.onrender.com"
+  baseURL: "https://YOUR-BACKEND-URL.onrender.com"
 });
 
 export default API;
