007f1ce7c7b8ac0c41815204248fa35b3de39429
gowthamitummala-2005 <23eg110c61@anurag.edu.in>
2026-04-05 10:54:43 +0530

    final login fix

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
 
-function Login({ onLogin, setPage }) {
+function Login({ setPage }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin =  () => {
-    const storedUser = localStorage.getItem("username"); 
-    const storedPassword = localStorage.getItem("password");
+  const handleLogin = () => {
+    const storedUser = localStorage.getItem("username");
+    const storedPass = localStorage.getItem("password");
 
     if (username === storedUser && password === storedPass) {
       alert("Login successful");
-      setPage("dashboard"); 
-    } else {  
+      setPage("dashboard");
+    } else {
       alert("Invalid username or password");
     }
   };
@@ -19,8 +19,15 @@ function Login({ onLogin, setPage }) {
   return (
     <div>
       <h2>Login</h2>
-      <input placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={handleLogin}>Login</button>
       <button onClick={() => setPage("register")}>Register</button>
     </div>
--- src/Register.jsx
+++ src/Register.jsx
@@ -7,15 +7,22 @@ function Register({ setPage }) {
   const handleRegister = () => {
     localStorage.setItem("username", username);
     localStorage.setItem("password", password);
-    alert("Registered successful");
+    alert("Registration successful");
     setPage("login");
   };
 
   return (
     <div>
       <h2>Register</h2>
-      <input placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={handleRegister}>Register</button>
       <button onClick={() => setPage("login")}>Back</button>
     </div>
